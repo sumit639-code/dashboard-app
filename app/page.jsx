981cf19950f3321d6a80c780b39bff0156031f76
Sumit639 <sumitkkumar639@gmail.com>
2023-11-02 23:02:38 +0530

    bg change

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '@/style/mainpage.css';
+const main = () => {
+  return (
+    <div className='main'>
+      <div className='main-box'>
+        <div></div>
+      </div>
+
+    </div>
+  )
+}
+
+export default main;
