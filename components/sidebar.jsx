b86308c8b588e95484b5287fa8b8901b6b2d58ad
Sumit639 <sumitkkumar639@gmail.com>
2023-11-03 09:51:07 +0530

    siedbar

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,16 +1,69 @@
-import React from 'react'
-import '@/style/sidebar.css'
-import Image from 'next/image'
+import React from "react";
+import "@/style/sidebar.css";
+import Image from "next/image";
 const sidebar = () => {
   return (
     <>
-        <div className="sidebar">
-            <span className="title">
-                <Image></Image>
-            </span>
-        </div>
+      <div className="sidebar">
+        <span className="title">
+          <Image
+            className="icons"
+            src="/icons/shopping-bag.png"
+            height={50}
+            width={50}
+          />
+          SHOP
+        </span>
+        <span className="menu">
+          <div className="menuitem">
+            <Image
+              className="icons2"
+              src="/icons/home.png"
+              height={50}
+              width={50}
+            />
+            Dashboard
+          </div>
+          <div className="menuitem">
+            <Image
+              className="icons2"
+              src="/icons/clipboard.png"
+              height={50}
+              width={50}
+            />
+            orders
+          </div>
+          <div className="menuitem">
+            <Image
+              className="icons2"
+              src="/icons/customer.png"
+              height={50}
+              width={50}
+            />
+            Customer
+          </div>
+          <div className="menuitem">
+            <Image
+              className="icons2"
+              src="/icons/product.png"
+              height={50}
+              width={50}
+            />
+            Products
+          </div>
+          <div className="menuitem">
+            <Image
+              className="icons2"
+              src="/icons/chart.png"
+              height={50}
+              width={50}
+            />
+            Analytics
+          </div>
+        </span>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default sidebar
+export default sidebar;
